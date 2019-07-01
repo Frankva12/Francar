4d621476896d6ed49654aa953c4b1cffda343744
CarolinaCabrera05 <37211200+CarolinaCabrera05@users.noreply.github.com>
2019-07-01 11:15:45 -0600

    model carrito

---
+++ francar/core/models/public/carrito.php
@@ -0,0 +1,158 @@
+<?php
+class Carrito extends Validator
+{
+	// Declaración de propiedades
+	private $id = null;
+	private $libro= null;
+	private $cliente = null;
+	private $cantidad = null;
+    private $precio = null;
+    private $imagen = null;
+	private $ruta = '../../../resources/img/carrito/';
+
+	// Métodos para sobrecarga de propiedades
+	public function setId($value)
+	{
+		if ($this->validateId($value)) {
+			$this->id = $value;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function getId()
+	{
+		return $this->id;
+    }
+
+    
+    public function setLibro($value)
+	{
+		if ($this->validateId($value)) {
+			$this->libro = $value;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function getLibro()
+	{
+		return $this->categoria;
+    }
+    
+
+
+    public function setCliente($value)
+	{
+		if ($this->validateId($value)) {
+			$this->cliente = $value;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function getCliente()
+	{
+		return $this->cliente;
+    }
+    
+
+
+
+	public function setCantidad($value)
+	{
+		if ($this->validateAlphanumeric($value, 1, 50)) {
+			$this->cantidad = $value;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function getCantidad()
+	{
+		return $this->cantidad;
+	}
+
+	public function setPrecio($value)
+	{
+		if ($this->validateMoney($value)) {
+			$this->precio = $value;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function getPrecio()
+	{
+		return $this->precio;
+	}
+
+	public function setImagen($file, $name)
+	{
+		if ($this->validateImageFile($file, $this->ruta, $name, 500, 500)) {
+			$this->imagen = $this->getImageName();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function getImagen()
+	{
+		return $this->imagen;
+	}
+
+	public function getRuta()
+	{
+		return $this->ruta;
+	}
+
+	//Metodos para el manejo del SCRUD
+	public function readProductosCategoria()
+	{
+		$sql = 'SELECT nombre_categoria, id_producto, imagen_producto, nombre_producto, descripcion_producto, precio_producto FROM productos INNER JOIN categorias USING(id_categoria) WHERE id_categoria = ? AND estado_producto = 1 ORDER BY nombre_producto';
+		$params = array($this->categoria);
+		return Database::getRows($sql, $params);
+	}
+
+	public function readProductos()
+	{
+		$sql = 'SELECT id_producto, imagen_producto, nombre_producto, descripcion_producto, precio_producto, nombre_categoria, estado_producto FROM productos INNER JOIN categorias USING(id_categoria) ORDER BY nombre_producto';
+		$params = array(null);
+		return Database::getRows($sql, $params);
+	}
+
+	public function getProducto()
+	{
+		$sql = 'SELECT id_producto, nombre_producto, descripcion_producto, precio_producto, imagen_producto, id_categoria, estado_producto FROM productos WHERE id_producto = ?';
+		$params = array($this->id);
+		return Database::getRow($sql, $params);
+    }
+    
+    public function createProducto()
+	{
+		$sql = 'INSERT INTO productos(nombre_producto, descripcion_producto, precio_producto, imagen_producto, estado_producto, id_categoria) VALUES(?, ?, ?, ?, ?, ?)';
+		$params = array($this->nombre, $this->descripcion, $this->precio, $this->imagen, $this->estado, $this->categoria);
+		return Database::executeRow($sql, $params);
+	}
+
+	public function updateProducto()
+	{
+		$sql = 'UPDATE productos SET nombre_producto = ?, descripcion_producto = ?, precio_producto = ?, imagen_producto = ?, estado_producto = ?, id_categoria = ? WHERE id_producto = ?';
+		$params = array($this->nombre, $this->descripcion, $this->precio, $this->imagen, $this->estado, $this->categoria, $this->id);
+		return Database::executeRow($sql, $params);
+	}
+
+	public function deleteProducto()
+	{
+		$sql = 'DELETE FROM compra WHERE id_compra = ?';
+		$params = array($this->id);
+		return Database::executeRow($sql, $params);
+	}
+}
+?>
--- francar/views/public/carrito.php
+++ francar/views/public/carrito.php
@@ -40,67 +40,27 @@
 		  <i class="material-icons">shopping_cart</i>
 		</a>
 	  </h3>
+
   <!--Se hace una tabla donde iran los libross que la persona pide, el costo de cada articulo, la cantidad y el total al pagar cuando llegue su pedido-->
-  <table class="responsive-table">
-    <thead>
-      <tr>
-        <th>Articulos</th>
-        <th>Precio</th>
-        <th>Cantidad</th>
-        <th></th>
-        <th>Total</th>
-        <th></th>
-      </tr>
-    </thead>
-    <!--Este es un ejemplo de como se verian los libross-->
-    <tbody>
-      <tr>
-        <td width="20%">
-          <img src="../../resources/img/hp5.jpg" width="150" height="150">
-        </td>
-        <td width="20%">$25.00</td>
-        <td width="100">
-          <input type="number" id="stepper1" name="stepper1" min="1" max="10" value="1" />
-        </td>
-        <td></td>
-        <td width="20%">$25.00</td>
-        <td>
-          <i class="material-icons">close</i>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <img src="../../resources/img/hp6.jpg" width="150" height="150">
-        </td>
-        <td>$25.00</td>
-        <td>
-          <input type="number" id="stepper1" name="stepper1" min="1" max="10" value="1" />
-        </td>
-        <td></td>
-        <td>$25.00</td>
-        <td>
-          <i class="material-icons">close</i>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <img src="../../resources/img/hp7.jpg" width="150" height="150">
-        </td>
-        <td>$25.00</td>
-        <td>
-          <input type="number" id="stepper1" name="stepper1" min="1" max="10" value="1" />
-        </td>
-        <td></td>
-        <td>$25.00</td>
-        <td>
-          <i class="material-icons">close</i>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <!--Se hace una tabla con el nombre de cada editorial-->
+  <div class="container">
+        <table class="highlight" id="tablaCarrito">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Libro</th>
+                    <th>Cantidad</th>
+                    <th>Cliente</th>
+                    <th>Precio</th>
+                </tr>
+            </thead>
+            <tbody id="tbody-read">
+            </tbody>
+        </table>
+    </div>
   <br>
-
-
+  <br>
+  <br>
   <br>
   <br>
   <br>
