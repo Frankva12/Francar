aff9a1fd487caf85b481951dfdc89cf1e3a1573e
Frank <stanleyvasconcelos0@gmail.com>
2019-10-17 12:29:46 -0600

    Update tabla.js

--- francar/resources/js/tabla.js
+++ francar/resources/js/tabla.js
@@ -1,32 +1,30 @@
-
 function initTable(id){
     var table =   $('#'+id).DataTable({
         "oLanguage":{
-            "sProcessing":     "Procesando...",
-            "sLengthMenu":     "Mostrar _MENU_ registros",
-            "sZeroRecords":    "No se encontraron resultados",
-            "sEmptyTable":     "Ningún dato disponible en esta tabla",
-            "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-            "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-            "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+            "sProcessing":     "Procesando.../Procesing...",
+            "sLengthMenu":     "Mostrar/Show _MENU_ registros/registers",
+            "sZeroRecords":    "No se encontraron resultados/ We don´t find results",
+            "sEmptyTable":     "Ningún dato disponible en esta tabla/ No data in this table",
+            "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros/ Showing registers from _START_ to _END_ of the total of _TOTAL_ registers",
+            "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros/ Showing registers from 0 to 10 from the total of 0",
+            "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)/ (filter from a total of _MAX_ registers)",
             "sInfoPostFix":    "",
-            "sSearch":         "Buscar:",
+            "sSearch":         "Buscar/Search:",
             "sUrl":            "",
             "sInfoThousands":  ",",
-            "sLoadingRecords": "Cargando...",
+            "sLoadingRecords": "Cargando.../Loading...",
             "oPaginate": {
-                "sFirst":    "Primero",
-                "sLast":     "Ultimo",
-                "sNext":     "Siguiente",
-                "sPrevious": "Anterior"
+                "sFirst":    "Primer/First",
+                "sLast":     "Ultimo/Last",
+                "sNext":     "Siguiente/Next",
+                "sPrevious": "Anterior/Before"
             },
             "oAria": {
-                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente: Activate for order the column in ascend form ",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente : Activate for order the column in descent form "
             }
         },
         retrieve: true
-       
     });
 }
 
